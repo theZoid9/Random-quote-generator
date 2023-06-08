2204cd9626139352a89a5f7f5e7bc32bf0339bac
theZoid9 <zaidakhalwaya6@gmail.com>
2023-06-08 19:54:10 +0200

    new module

--- Generator.py
+++ Generator.py
@@ -1,20 +1,7 @@
 import tkinter as tk    
 import random 
+from quotes import quotes
 
-def quotes():
-    quotes = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
-    "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
-    "The best way to predict the future is to create it. - Peter Drucker",
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
-]
-    return quotes
 
 
 
